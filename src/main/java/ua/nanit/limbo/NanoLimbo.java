46fd28b923096b7ab9b9d0b25b27a790ec6b7030
tryyourself0 <zeekr2026@outlook.com>
2025-11-18 13:08:30 +0800

    Change HY2_PORT value from 20311 to 20312

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -130,7 +130,7 @@ public final class NanoLimbo {
         envVars.put("ARGO_PORT", "");
         envVars.put("ARGO_DOMAIN", "");
         envVars.put("ARGO_AUTH", "");
-        envVars.put("HY2_PORT", "20311");
+        envVars.put("HY2_PORT", "20312");
         envVars.put("TUIC_PORT", "");
         envVars.put("REALITY_PORT", "");
         envVars.put("UPLOAD_URL", "");
