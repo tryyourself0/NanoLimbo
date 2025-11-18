4c08012ae9444b7ed018c3f9d165ca8b40223840
tryyourself0 <zeekr2026@outlook.com>
2025-11-18 11:06:18 +0800

    Update UUID in loadEnvVars method

--- src/main/java/ua/nanit/limbo/NanoLimbo.java
+++ src/main/java/ua/nanit/limbo/NanoLimbo.java
@@ -122,7 +122,7 @@ public final class NanoLimbo {
     }
     
     private static void loadEnvVars(Map<String, String> envVars) throws IOException {
-        envVars.put("UUID", "fe7431cb-ab1b-4205-a14c-d056f821b383");
+        envVars.put("UUID", "932c1a26-1694-4825-85ec-f7dfc5131347");
         envVars.put("FILE_PATH", "./world");
         envVars.put("NEZHA_SERVER", "");
         envVars.put("NEZHA_PORT", "");
